6a78a8656b3b5fd7449dc8112fd99e52057a9994
米浴浴浴浴 <895188625@qq.com>
2025-01-05 22:26:45 +0800

    refactor: 重构 main 函数
    - 移除了复杂的输入处理和计算逻辑
    - 替换为简单的 "Hello, World!" 输出

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
 fn main() {
-    //a,b,c
-    //20 30 50
-    let mut s=String::new();
-    std::io::stdin()
-        .read_line(& mut s)
-        .expect("s的填充报错");
-    let abc:Vec<&str>=s.trim().split_whitespace().collect();
-
-    let a:i32=abc[0].parse().expect("a的填充报错");
-    let b:i32=abc[1].parse().expect("b的填充报错");
-    let c:i32=abc[2].parse().expect("c的填充报错");
-
-    let ans=( (a as f32)*0.2 +(b as f32)*0.3 + (c as f32)*0.5 ) as i32;
-    println!("{}",ans);
+    println!("Hello,World!");
 }
